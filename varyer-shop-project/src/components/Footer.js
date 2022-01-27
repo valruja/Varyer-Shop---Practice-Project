f136486045a47d80775c70c5f56b5a5182503922
valruja <61010136+valruja@users.noreply.github.com>
2022-01-27 17:31:29 -0400

    advances in footer

--- varyer-shop-project/src/components/Footer.js
+++ varyer-shop-project/src/components/Footer.js
@@ -14,10 +14,36 @@ const Footer = () => {
                 <a>HEY@VARYER.COM</a>
             </div>
             <div class='webpage-switch-container'>
-
+                <div class='circles-container'>
+                    <div>
+                        <span class="dot1"></span>
+                        <p class='title-dot1'>Varyer</p>
+                        <p class='subtitle-dot1'>YOU ARE HERE</p>
+                    </div>
+                    <div>
+                        <span class="dot2"></span>
+                        <p class='title-dot2'>Studio</p>
+                        <p class='subtitle-dot2'>GO HERE</p>
+                    </div>
+                </div>
+                <div>
+                    <span class="square"></span>
+                </div>
             </div>
             <div class='terms-container'>
-
+                <div class='home-footer-container'>
+                    <p>Object 1</p>
+                </div>
+                <div class='conditions-container'>
+                    <a>2022 VARYER LLC</a>
+                    <a>PRIVACY</a>
+                    <a>SHIPPING</a>
+                    <a>TERMS OF USE</a>
+                    <a>RETURNS AND EXCHANGES</a>
+                </div>
+                <div class='flowers-container'>
+                    <p>Object 1</p>
+                </div>
             </div>
 
         </div>
