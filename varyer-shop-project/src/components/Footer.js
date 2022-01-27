61abc148a506557c3603fd8a5b2df65cdf2bc72c
valruja <61010136+valruja@users.noreply.github.com>
2022-01-26 23:41:40 -0400

    footer created

---
+++ varyer-shop-project/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+//Import css
+import '../css/Footer.css'
+
+const Footer = () => {
+    return (
+        <div class='footer-container'>
+            <div class='social-media-container'>
+                <a>INSTAGRAM</a>
+                <a>SPOTIFY</a>
+                <a>TWITTER</a>
+                <a>INFO</a>
+                <a>HEY@VARYER.COM</a>
+            </div>
+            <div class='webpage-switch-container'>
+
+            </div>
+            <div class='terms-container'>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
